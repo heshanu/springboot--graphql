d41988aa004c0048d02fff4f043974d0409bd54d
Heshan Umayanga <38934645+heshanu@users.noreply.github.com>
2024-02-28 19:20:29 +0530

    Add files via upload

---
+++ GraphQL-Client-To-Call-GraphQL-Mutation/src/main/java/com/infybuzz/service/ClientService.java
@@ -0,0 +1,81 @@
+package com.infybuzz.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.infybuzz.request.CreateStudentRequest;
+import com.infybuzz.response.StudentResponse;
+
+import graphql.kickstart.spring.webclient.boot.GraphQLRequest;
+import graphql.kickstart.spring.webclient.boot.GraphQLResponse;
+import graphql.kickstart.spring.webclient.boot.GraphQLWebClient;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	GraphQLWebClient graphQLWebClient;
+
+	public StudentResponse getStudent (Integer id) {
+		Map<String, Object> variables = new HashMap<>();
+		variables.put("id", id);
+		
+		String queryStr = "query student($id : Int) {\r\n"
+				+ "  student(id : $id) {\r\n"
+				+ "    id\r\n"
+				+ "    firstName\r\n"
+				+ "    lastName\r\n"
+				+ "    email\r\n"
+				+ "    street\r\n"
+				+ "    city\r\n"
+				+ "    learningSubjects (subjectNameFilter : All) {\r\n"
+				+ "      id\r\n"
+				+ "      subjectName\r\n"
+				+ "      marksObtained\r\n"
+				+ "    }\r\n"
+				+ "    fullName\r\n"
+				+ "  } \r\n"
+				+ "}";
+		
+		GraphQLRequest request = GraphQLRequest.builder()
+				.query(queryStr).variables(variables).build();
+		
+		GraphQLResponse graphQLResponse = graphQLWebClient.post(request).block();
+		
+		return graphQLResponse.get("student", StudentResponse.class);
+	}
+	
+	public StudentResponse createStudent(
+			CreateStudentRequest createStudentRequest) {
+		
+		Map<String, Object> variables = new HashMap<>();
+		variables.put("createStudentRequest", createStudentRequest);
+		
+		String mutationStr = "mutation createStudent ($createStudentRequest : CreateStudentRequest) {\r\n"
+				+ "  createStudent (createStudentRequest : $createStudentRequest) {\r\n"
+				+ "    id\r\n"
+				+ "    firstName\r\n"
+				+ "    lastName\r\n"
+				+ "    email\r\n"
+				+ "    street\r\n"
+				+ "    city\r\n"
+				+ "    learningSubjects(subjectNameFilter : All) {\r\n"
+				+ "      id\r\n"
+				+ "      subjectName\r\n"
+				+ "      marksObtained\r\n"
+				+ "    }\r\n"
+				+ "    fullName\r\n"
+				+ "  }\r\n"
+				+ "}";
+		
+		GraphQLRequest request = GraphQLRequest.builder()
+				.query(mutationStr).variables(variables).build();
+		
+		GraphQLResponse graphQLResponse = graphQLWebClient.post(request).block();
+		
+		return graphQLResponse.get("createStudent", StudentResponse.class);
+	}
+}
